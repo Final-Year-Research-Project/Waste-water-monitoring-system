74f048214742e85014096f9166eacd6c041dd86b
Osanda Randunu <it20082888@my.sliit.lk>
2023-09-02 01:12:01 +0530

    back end completed
    implemented backend functions to connect to database and frontend

---
+++ arduno_to_db/flowrate.php
@@ -0,0 +1,23 @@
+<?php
+class flowrate{
+ public $link='';
+ function __construct($flow_in, $flow_out){
+  $this->connect();
+  $this->storeInDB($flow_in, $flow_out);
+ }
+ 
+ function connect(){
+  $this->link = mysqli_connect('localhost','root','') or die('Cannot connect to the DB');
+  mysqli_select_db($this->link,'temphumidnew') or die('Cannot connect the DB');
+ }
+ 
+ function storeInDB($flow_in, $flow_out){
+  $query = "insert into flowrate set flow_out='".$flow_out."', flow_in='".$flow_in."',temperature='".$temperature."'";
+  $result = mysqli_query($this->link,$query) or die('Errant query:  '.$query);
+ }
+ 
+}
+if($_GET['flow_in'] != '' and  $_GET['flow_out'] != '' and $_GET['temperature'] != ''){
+ $flowrate=new flowrate($_GET['flow_in'],$_GET['flow_out'],$_GET['temperature']);
+}
+?>
